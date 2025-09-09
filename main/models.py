0e9aedf88df7e205800931f641d2348f29eda4f1
rpmdim <raden.pandji@ui.ac.id>
2025-09-09 23:01:19 +0700

    changing models.py

--- main/models.py
+++ main/models.py
@@ -1,32 +1,18 @@
 import uuid
 from django.db import models
 
-class News(models.Model):
-    CATEGORY_CHOICES = [
-        ('transfer', 'Transfer'),
-        ('update', 'Update'),
-        ('exclusive', 'Exclusive'),
-        ('match', 'Match'),
-        ('rumor', 'Rumor'),
-        ('analysis', 'Analysis'),
-    ]
-    
+class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    news_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=20, default='update')
     is_featured = models.BooleanField(default=False)
+    stock = models.IntegerField(default=0)
+    rating = models.FloatField(default=0.0)
+    brand = models.CharField(max_length=35, blank=True, null=True)
+    size = models.CharField(max_length=10, blank=True, null=True)
     
     def __str__(self):
-        return self.title
-    
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
-        
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
+        return self.name
